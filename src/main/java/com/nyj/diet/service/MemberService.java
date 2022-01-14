afeb9c5c944d6715664830a7865f68dfd287750e
NamYeJi <dkdlelql123@naver.com>
2022-01-14 14:54:17 +0900

    transational 추가, member 엔티티 수정

--- src/main/java/com/nyj/diet/service/MemberService.java
+++ src/main/java/com/nyj/diet/service/MemberService.java
@@ -11,9 +11,11 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @AllArgsConstructor
+@Transactional(readOnly = true)
 public class MemberService implements UserDetailsService {
 
     private final MemberRepository memberRepository;
@@ -45,6 +47,7 @@ public class MemberService implements UserDetailsService {
      * @param memberSaveForm
      */
 
+    @Transactional
     public void save(MemberSaveForm memberSaveForm) throws IllegalStateException{
 
         isDuplicateMember(
