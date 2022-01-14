059db44a41b0ee9406b0f58e4f19faf1b61085b8
NamYeJi <dkdlelql123@naver.com>
2022-01-14 14:44:45 +0900

    회원가입 로직 구현

---
+++ src/main/java/com/nyj/diet/dto/MemberSaveForm.java
@@ -0,0 +1,13 @@
+package com.nyj.diet.dto;
+
+import lombok.Data;
+
+@Data
+public class MemberSaveForm {
+
+    private String loginId;
+    private String loginPw;
+    private String name;
+    private String nickname;
+    private String email;
+}
--- src/main/java/com/nyj/diet/service/MemberService.java
+++ src/main/java/com/nyj/diet/service/MemberService.java
@@ -1,11 +1,15 @@
 package com.nyj.diet.service;
 
 import com.fasterxml.classmate.MemberResolver;
+import com.nyj.diet.config.Role;
 import com.nyj.diet.dao.MemberRepository;
+import com.nyj.diet.domain.Member;
+import com.nyj.diet.dto.MemberSaveForm;
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -17,6 +21,50 @@ public class MemberService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return memberRepository.findByLoginId(username).get();
+    }
+
+    /**
+     * 회원 중복 체크
+     * @param loginId
+     * @param nickname
+     * @param email
+     */
+    public void isDuplicateMember(String loginId, String nickname, String email){
+
+        if(memberRepository.existByLoginId(loginId)){
+            throw new IllegalStateException("이미 존재하는 아이디 입니다");
+        }else if(memberRepository.existByNickname(nickname)){
+            throw new IllegalStateException("이미 존재하는 아이디 입니다");
+        }else if(memberRepository.existByEmail(email)){
+            throw new IllegalStateException("이미 존재하는 이메일 입니다.");
+        }
+    }
+
+    /**
+     * 회원가입
+     * @param memberSaveForm
+     */
+
+    public void save(MemberSaveForm memberSaveForm) throws IllegalStateException{
+
+        isDuplicateMember(
+                memberSaveForm.getLoginId(),
+                memberSaveForm.getNickname(),
+                memberSaveForm.getEmail()
+        );
+
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+
+        Member member = Member.createMember(
+                memberSaveForm.getLoginId(),
+                bCryptPasswordEncoder.encode(memberSaveForm.getLoginPw()),
+                memberSaveForm.getName(),
+                memberSaveForm.getNickname(),
+                memberSaveForm.getEmail(),
+                Role.MEMBER
+        );
+
+        memberRepository.save(member);
     }
 
 }
