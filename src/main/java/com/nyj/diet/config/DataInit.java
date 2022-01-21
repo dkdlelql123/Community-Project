0fe3e49246ae663f8ae66f0512c253d0a0366ed9
NamYeJi <dkdlelql123@naver.com>
2022-01-21 15:23:44 +0900

    관리자 계정 생성

--- src/main/java/com/nyj/diet/config/DataInit.java
+++ src/main/java/com/nyj/diet/config/DataInit.java
@@ -1,2 +1,50 @@
-package com.nyj.diet.config;public class DataInit {
+package com.nyj.diet.config;
+
+import com.nyj.diet.dao.MemberRepository;
+import com.nyj.diet.domain.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+
+// 스프링부트
+// component, bean 차이점 알아보기
+@Component
+@RequiredArgsConstructor
+public class DataInit {
+
+    private final InitService initService;
+
+    // 서버를 (재)시작 할때 postConstruct 먼저 실행해줌.
+    @PostConstruct
+    public void init(){
+        initService.initAdmin();
+    }
+
+    @Component
+    @Service
+    @RequiredArgsConstructor
+    static class InitService{
+        private final MemberRepository memberRepository;
+
+        //메소드 생성
+        public void initAdmin(){
+            // 비밀번호 encoder
+            BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+
+            Member admin = Member.createMember(
+                    "admin",
+                    bCryptPasswordEncoder.encode("admin"),
+                    "관리자",
+                    "관리자",
+                    "admin@admin.com",
+                    Role.ADMIN
+            );
+
+            memberRepository.save(admin);
+        }
+    }
+
 }
