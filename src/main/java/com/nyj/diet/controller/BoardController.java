7f137a07a91f50c5f5caf797adcfa8eb203d533d
NamYeJi <dkdlelql123@naver.com>
2022-01-21 14:13:20 +0900

    board 리스트 구현

--- src/main/java/com/nyj/diet/controller/BoardController.java
+++ src/main/java/com/nyj/diet/controller/BoardController.java
@@ -1,5 +1,6 @@
 package com.nyj.diet.controller;
 
+import com.nyj.diet.domain.Board;
 import com.nyj.diet.dto.Board.BoardSaveForm;
 import com.nyj.diet.service.BoardService;
 import lombok.RequiredArgsConstructor;
@@ -8,23 +9,40 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 @RequiredArgsConstructor
 public class BoardController {
     private final BoardService boardService;
 
+    // 작성하기
     @GetMapping("/boards/add")
     public String showAddBoard(Model model){
         model.addAttribute("boardSaveForm", new BoardSaveForm());
 
         return "usr/board/add";
     }
+
     @PostMapping("/boards/add")
     public String doAddBoard(BoardSaveForm boardSaveForm){
         boardService.save(boardSaveForm);
 
         return "redirect:/";
     }
+
+    // 리스트
+    @GetMapping("/boards")
+    public String showBoardList(Model model){
+        List<Board> boardList = boardService.findAll();
+        
+        // HTML로 보낼 수 있게 됨
+        model.addAttribute("boardList", boardList);
+        
+        return "/usr/board/list";
+    }
+
+
 }
 
 
--- src/main/java/com/nyj/diet/service/BoardService.java
+++ src/main/java/com/nyj/diet/service/BoardService.java
@@ -6,18 +6,24 @@ import com.nyj.diet.dto.Board.BoardSaveForm;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class BoardService {
 
     private final BoardRepository boardRepository;
 
-    public void save(BoardSaveForm boardSaveForm){
+    public void save(BoardSaveForm boardSaveForm) {
         Board board = Board.createBoard(
                 boardSaveForm.getName(),
                 boardSaveForm.getDetail()
         );
 
         boardRepository.save(board);
+    }
+
+    public List<Board> findAll() {
+        return boardRepository.findAll();
     }
 }
