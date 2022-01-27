14a59498ead9a19794bde06903ccf6150934658e
NamYeJi <dkdlelql123@naver.com>
2022-01-27 14:17:51 +0900

    게시판 수정 개선

--- src/main/java/com/nyj/diet/dto/Board/BoardModifyForm.java
+++ src/main/java/com/nyj/diet/dto/Board/BoardModifyForm.java
@@ -1,8 +1,10 @@
 package com.nyj.diet.dto.Board;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class BoardModifyForm {
     private String name;
     private String detail;
