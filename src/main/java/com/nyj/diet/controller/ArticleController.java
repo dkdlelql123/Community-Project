eb9f0ee32f9f577ac4402b3429a5518366dbb09d
NamYeJi <dkdlelql123@naver.com>
2022-02-09 15:44:35 +0900

    게시글 수정 에러메세지 렌더링

--- src/main/java/com/nyj/diet/controller/ArticleController.java
+++ src/main/java/com/nyj/diet/controller/ArticleController.java
@@ -86,11 +86,13 @@ public class ArticleController {
     @GetMapping("/articles/modify/{id}")
     public String showModify(@PathVariable(name="id") Long id, Model model){
         try{
-            ArticleDTO article = articleService.getArticle(id);
+            ArticleDTO findArticle = articleService.getArticle(id);
 
-            model.addAttribute(
-                    "article", article
-            );
+            model.addAttribute("boardName", findArticle.getBoardName());
+            model.addAttribute("boardId", findArticle.getBoardId());
+            model.addAttribute("articleId", findArticle.getId());
+            model.addAttribute("articleModifyForm", new ArticleModifyForm(findArticle) );
+
             return "usr/article/modify";
 
         }catch (Exception e){
@@ -99,7 +101,18 @@ public class ArticleController {
     }
 
     @PostMapping("/articles/modify/{id}")
-     public String doModify(@PathVariable(name="id") Long id, ArticleModifyForm articleModifyForm, Principal principal){
+     public String doModify(@PathVariable(name="id") Long id,@Validated ArticleModifyForm articleModifyForm, BindingResult bindingResult, Principal principal, Model model){
+
+        if(bindingResult.hasErrors()){
+            ArticleDTO findArticle = articleService.getArticle(id);
+
+            model.addAttribute("boardName", findArticle.getBoardName());
+            model.addAttribute("boardId", findArticle.getBoardId());
+            model.addAttribute("articleId", findArticle.getId());
+
+            return "usr/article/modify";
+        }
+
         try{
 
             ArticleDTO findArticle = articleService.getArticle(id);
--- src/main/java/com/nyj/diet/dto/article/ArticleModifyForm.java
+++ src/main/java/com/nyj/diet/dto/article/ArticleModifyForm.java
@@ -2,14 +2,31 @@ package com.nyj.diet.dto.article;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
 
 // 어떤 인자를 받는 생성자로 만들어주겠다.
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class ArticleModifyForm {
+    
+    @NotBlank(message = "제목을 입력해주세요.")
     private String title;
+    
+    @NotBlank(message = "내용을 입력해주세요.")
     private String body;
 
     private Long board_id;
+
+    private String board_name;
+
+    public ArticleModifyForm(ArticleDTO articleDTO){
+        this.title = articleDTO.getTitle();
+        this.body = articleDTO.getBody();
+
+        this.board_id = articleDTO.getBoardId();
+    }
 }
