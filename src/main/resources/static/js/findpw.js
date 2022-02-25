5d0e0a6669407fa909c5c95265c437e450c68907
NamYeJi <dkdlelql123@naver.com>
2022-02-25 15:33:42 +0900

    비밀버호재발급 - 메일 갈때 로딩시 스피너 ui추가

--- src/main/resources/static/js/findpw.js
+++ src/main/resources/static/js/findpw.js
@@ -1,6 +1,11 @@
 let token = document.querySelector("meta[name='_csrf']").getAttribute("content");
 //let header = document.querySelector("meta[name='_csrf_header']").getAttribute("content");
 
+let progress = document.querySelector("#progress");
+let findContent = document.querySelector("#findContent");
+
+progress.hidden = true;
+
 const findForm = document.querySelector("#findForm");
 findForm.addEventListener("submit", findPw);
 
@@ -21,6 +26,7 @@ async function findPw(e){
         return;
     }
 
+
     let data = {
         method : "POST",
         body: JSON.stringify(
@@ -33,8 +39,10 @@ async function findPw(e){
             'Content-Type' : 'application/json',
             'X-CSRF-TOKEN' : token
         }
-
     };
+
+    findContent.hidden = true;
+    progress.hidden = false;
 
     await fetch(
     "http://localhost:8086/mails/find/pw", data
@@ -46,8 +54,13 @@ async function findPw(e){
         (data) => {
 
             if( !data ){
+
+                findContent.hidden = false;
+                progress.hidden = true;
+
                 alert("이메일 발송 실패, 이메일을 확인해 주시기 바랍니다.");
                 return;
+
             } else {
                 alert("발급된 임시 비밀번호를 입력하신 이메일로 보냈습니다.");
                 window.location.replace("http://localhost:8086/");
@@ -55,6 +68,9 @@ async function findPw(e){
         }
     ).catch(
         (error) => {
+            findContent.hidden = false;
+            progress.hidden = true;
+
             console.log(error);
             alert("메일 발송에 실패하였습니다. 이메일 혹은 아이디를 확인하여 주시기 바랍니다.");
         }
